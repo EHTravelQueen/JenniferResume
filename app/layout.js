eecb29658c2dd4dafb36e813ee680dd5eb05137f
EHTravelQueen <ldrbeauty@gmail.com>
2026-04-30 18:40:59 -0500

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Jennifer Higgs Stewart | Online Resume",
+  description: "Interactive online resume for Jennifer Higgs Stewart."
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
